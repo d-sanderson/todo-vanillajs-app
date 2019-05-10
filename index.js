ecd298ff8477351d3ee208ddd58fb72e37b1ca3b
dsanderson90 <dsanderson3@cnm.edu>
2019-05-09 18:40:29 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1. it should ave a place to store todos
+// 2. it should have a way to display todos
+// 3 .it should have a way to add new todos
+// 4 .it should have a way to edit a todos
+// 5. it should have a way to delete a todo
+
